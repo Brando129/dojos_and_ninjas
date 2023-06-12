7e8beac90f72aa33049e13df0d4a09a1bd686f03
Brandon Colangelo <brandoncancode@gmail.com>
2023-06-12 12:13:02 -0400

    dojo class, added new route, made index page

--- flask_app/controllers/dojos.py
+++ flask_app/controllers/dojos.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,redirect,request
-
 from flask_app import app
+from flask_app.models.dojo import Dojo
 
 
 from flask_app.models.dojo import Dojo
@@ -8,4 +8,9 @@ from flask_app.models.dojo import Dojo
 # Home Page
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return redirect("/dojos")
+
+@app.route('/dojos')
+def dojos():
+    dojos = Dojo.get_all()
+    return render_template("index.html", all_dojos = dojos)
