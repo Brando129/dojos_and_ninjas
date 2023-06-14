4076c2672933f422c9a46ec774be67c6ddb8d1bf
Brandon Colangelo <brandoncancode@gmail.com>
2023-06-14 12:14:47 -0400

    created class method for updating edited ninja

--- flask_app/models/models_ninja.py
+++ flask_app/models/models_ninja.py
@@ -17,15 +17,22 @@ class Ninja:
                 VALUES (%(first_name)s, %(last_name)s, %(age)s, %(dojo_id)s);"""
         return connectToMySQL(db).query_db(query, data)
 
-    # classmethod for deleting a ninja
-    @classmethod
-    def destroy(cls, data):
-        query = "DELETE FROM ninjas WHERE id = %(id)s;"
-        return connectToMySQL(db).query_db(query, data)
-
     #classmethod for selecting a ninja to edit
     @classmethod
     def get_one(cls, data):
         query = "SELECT * FROM ninjas WHERE id = %(id)s";
         result = connectToMySQL(db).query_db(query, data)
         return cls(result[0])
+
+    #classmethod for updating the edited ninja
+    @classmethod
+    def update(cls, data):
+        query = """UPDATE ninjas INTO first_name=%(first_name)s, last_name=%(last_name)s,
+                age=%(age)s, updated_at=NOW() WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query, data)
+
+    # classmethod for deleting a ninja
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM ninjas WHERE id = %(id)s;"
+        return connectToMySQL(db).query_db(query, data)
